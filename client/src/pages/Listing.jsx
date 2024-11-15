56ffb16d57b8068979bd6c3d95d4388f3c985b40
Raj Nayan <122800487+rajnayan07@users.noreply.github.com>
2024-11-15 14:50:43 +0530

    Update Listing.jsx

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+ import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import SwiperCore from 'swiper';
@@ -95,7 +95,7 @@ export default function Listing() {
          {listing.offer
          ? listing.discountPrice.toLocaleString('en-IN')
            : listing.regularPrice.toLocaleString('en-IN')}
-           {listing.type === 'rent' && ' / month'}
+           {/* {listing.type === 'rent' && ' / month'} */}
             
             </p>
             <p className='flex items-center mt-6 gap-2 text-slate-600  text-sm'>
@@ -104,7 +104,7 @@ export default function Listing() {
             </p>
             <div className='flex gap-4'>
               <p className='bg-red-900 w-full max-w-[200px] text-white text-center p-1 rounded-md'>
-                {listing.type === 'rent' ? 'For Rent' : 'For Sale'}
+                {listing.type === 'buy' ? 'For buy' : 'For Sale'}
               </p>
               {listing.offer && (
                 <p className='bg-green-900 w-full max-w-[200px] text-white text-center p-1 rounded-md'>
@@ -117,26 +117,26 @@ export default function Listing() {
               {listing.description}
             </p>
             <ul className='text-green-900 font-semibold text-sm flex flex-wrap items-center gap-4 sm:gap-6'>
-              <li className='flex items-center gap-1 whitespace-nowrap '>
+              {/* <li className='flex items-center gap-1 whitespace-nowrap '>
                 <FaBed className='text-lg' />
                 {listing.bedrooms > 1
                   ? `${listing.bedrooms} beds `
                   : `${listing.bedrooms} bed `}
-              </li>
-              <li className='flex items-center gap-1 whitespace-nowrap '>
+              </li> */}
+              {/* <li className='flex items-center gap-1 whitespace-nowrap '>
                 <FaBath className='text-lg' />
                 {listing.bathrooms > 1
                   ? `${listing.bathrooms} baths `
                   : `${listing.bathrooms} bath `}
-              </li>
-              <li className='flex items-center gap-1 whitespace-nowrap '>
+              </li> */}
+              {/* <li className='flex items-center gap-1 whitespace-nowrap '>
                 <FaParking className='text-lg' />
                 {listing.parking ? 'Parking spot' : 'No Parking'}
-              </li>
-              <li className='flex items-center gap-1 whitespace-nowrap '>
+              </li> */}
+              {/* <li className='flex items-center gap-1 whitespace-nowrap '>
                 <FaChair className='text-lg' />
                 {listing.furnished ? 'Furnished' : 'Unfurnished'}
-              </li>
+              </li> */}
             </ul>
             {currentUser && listing.userRef !== currentUser._id && !contact && (
               <button
