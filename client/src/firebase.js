7227c50d8aaa92e98cbed07fa78be09ee8e713f0
Raj Nayan <122800487+rajnayan07@users.noreply.github.com>
2024-11-15 13:30:17 +0530

    Add files via upload

---
+++ client/src/firebase.js
@@ -0,0 +1,35 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+//   authDomain: "carkharido-c3dfe.firebaseapp.com",
+//   projectId: "carkharido-c3dfe",
+//   storageBucket: "carkharido-c3dfe.firebasestorage.app",
+//   messagingSenderId: "1061218552420",
+//   appId: "1:1061218552420:web:3ad8403939fb9cc3d5ad71"
+// };
+
+// // Initialize Firebase
+// export const app = initializeApp(firebaseConfig);
+
+// // Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey:import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "homevista1-45a85.firebaseapp.com",
+  projectId: "homevista1-45a85",
+  storageBucket: "homevista1-45a85.appspot.com",
+  messagingSenderId: "1043662371982",
+  appId: "1:1043662371982:web:d40746bfddc994dfcb9ddf"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
